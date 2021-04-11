4dcfe5dc4d04afcdbc1ae977d6384ef02c7e1a3a
hikager <lluis22.ml@gmail.com>
2021-04-11 17:15:08 +0200

    - Adding a new view for testing GET method (simple view) - Adding a url.py in movies for mapping views - Adding the url file into main urls.py (using include func) - Adding in settings_app the new app movies

--- metflix_remake/movies/views.py
+++ metflix_remake/movies/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def index(response):
+    return HttpResponse('First movie view in metflix!')
