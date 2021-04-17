42d8ada4d1cea811be272b76ca91adb519492577
hikager <lluis22.ml@gmail.com>
2021-04-17 14:22:42 +0200

    Adding new database settings 	- Adding a new dictionary of MySQL setting for django 	Using:  'ENGINE': 'django.db.backends.mysql' and some basic data for loggin
    	Commentaries(additional info):
    	- Making: pip install mysql-python for connector dependency
    	I have some problems so I had to install in my machine:
    		- sudo apt install libmysqlclient-dev
    		- sudo apt install python-dev
    	After this, I was able to install the connector for MySQL.

--- metflix_remake/metflix_remake/settings.py
+++ metflix_remake/metflix_remake/settings.py
@@ -73,10 +73,7 @@ WSGI_APPLICATION = 'metflix_remake.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': DJANGO['database_cnf'],
 }
 
 # Password validation
