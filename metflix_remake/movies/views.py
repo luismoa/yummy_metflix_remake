925600fad0256a851fb06a448a7e583ed5046b92
hikager <lluis22.ml@gmail.com>
2021-04-24 14:05:31 +0200

    18: Adding a new view to movies 	- Creating an arraneged template directory 	- Using a class for Movie view instead of using functions

--- metflix_remake/movies/views.py
+++ metflix_remake/movies/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Movie
+from django.views import View
+
+"""def index(response):
+    return HttpResponse('First movie view in metflix!')"""
 
 
-def index(response):
-    return HttpResponse('First movie view in metflix!')
+class MovieView(View):
+    model = Movie
+
+    # Queries
+    all_movies = model.objects.all()
+
+    def get(self, request):
+        context = {
+            'movie_list': list(self.all_movies)
+        }
+        return render(request, 'movies.html', context=context)
