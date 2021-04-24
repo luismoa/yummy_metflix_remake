ac0664412f91464caef965de401c1691d9192538
hikager <lluis22.ml@gmail.com>
2021-04-24 18:52:59 +0200

    Removing commentaries

--- docs/querysets/insert_movies_app_blueprint.py
+++ docs/querysets/insert_movies_app_blueprint.py
@@ -1,12 +1,10 @@
 """
-This is a sample ("blueprint") queries for resources app
+This is a sample ("blueprint") queries for movies app
 - It will be much easy to make some "copy-paste" and generate new records for testing the database
 
 """
 from movies.models import Director, Movie, Actor, MovieActor
 
-# I could use a dictionary, then it will be easily to find which position is a movie(by title) or other thing.
-# Then when we add a new record to the dictionary we just need (when a FK) to select dictionary key value instead of a position
 directors = []
 movies = []
 actors = []
@@ -25,6 +23,3 @@ actors.append(
 movie_actors.append(
     MovieActor.objects.create(movie=movies[0], actor=actors[0], role_field='superman', is_main_character=True)
 )
-
-# With a loop make each INSERT query and print out "new actor named added to ..." for example
-# Add some functions or use a class to arrange this blueprint
