00b36996dfbe807537acda568fecc5574b499979
hikager <lluis22.ml@gmail.com>
2021-05-23 23:53:53 +0200

    Adding a new dir for the API 	- Adding views for handling basic GET and POST 	- Adding the endpoint: /movies/api

--- metflix_remake/movies/urls.py
+++ metflix_remake/movies/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
+from .api.views import MovieApiView
 
 urlpatterns = [
-    path('', views.MovieView.as_view(), name='index')
+    path('', views.MovieView.as_view(), name='index'),
+    path('api/', MovieApiView.as_view())
 ]
