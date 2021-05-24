3a58150bd5619a99bfadb06c50c9d21858f9a252
hikager <lluis22.ml@gmail.com>
2021-05-24 14:12:12 +0200

    Fixing counting movie list counter 	- Updating counting asyn by ajax 	- Adding an element in main.html to wrap all the async content

--- metflix_remake/movies/static/js/update_movie_list.js
+++ metflix_remake/movies/static/js/update_movie_list.js
@@ -1,40 +1,45 @@
- const interval_wait_ajax_to_request=2000;
+const INTERVAL_AJAX_WAIT_TO_REQUEST = 2000;
+var IS_CREATED_MOVIE_COUNTER = false;
+function updateMovieList(updated_movies)
+{
+  var items = [];
+  for (var i in updated_movies) {
+      var item = updated_movies[i];
+      items.push('<li id="movie_' + item.id + '">' + item.title + '</li>');
+  }
+  $('#ul_movie').html(items.join(''));
+}
 
-    function updateMovieList(updated_movies)
-    {
-      var items = [];
-      for (var i in updated_movies) {
-          var item = updated_movies[i];
-          items.push('<li id="' + item.id + '">' + item.title + '</li>');
-      }
-      $('#ul_movie').html(items.join(''));
-    }
+ function createMovieCounterElements()
+{
+    var spanMovieCounting = document.createElement("span");
+    $('.total-movies-wrapper').text("Total movies:");
+    spanMovieCounting.setAttribute("class","movie-counting");
+    document.querySelector(".movie-data-ajax").appendChild(spanMovieCounting)
+    IS_CREATED_MOVIE_COUNTER = true
+}
 
-    function updateMovieListCunter(updated_movies)
-    {
-      var movies = updated_movies.length
-      $('#movie-counter-ajax').innerHTML =movies
-    }
+function updateMovieListCounter(updated_movies)
+{
+  var movies = updated_movies.length;
+  console.log(movies)
+  if (!IS_CREATED_MOVIE_COUNTER){
+    createMovieCounterElements()
+   }
+  $(".movie-data-ajax span.movie-counting").text(movies);
+}
 
-   $(document).ready(function(){
+/*AJAX request for updating our movie list asynchronous */
+$(document).ready(function(){
     setInterval(function(){
-     $.ajax({
-    url:"/movies/api/",
-    type:"get",
-    dataType:"json",
-    success: function(response){
-        updateMovieList(response);
-        updateMovieListCunter(response);
-        console.log(response);
-    }
-   });
-    }, interval_wait_ajax_to_request);
+    $.ajax({
+        url:"/movies/api/",
+        type:"get",
+        dataType:"json",
+        success: function(response){
+            updateMovieList(response);
+            updateMovieListCounter(response);
+        }
 });
-
-
-
-
-
-
-
-
+}, INTERVAL_AJAX_WAIT_TO_REQUEST);
+});
