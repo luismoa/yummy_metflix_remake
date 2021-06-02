c61c514a79bf28120b380704cf3c325947db7f78
hikager <lluis22.ml@gmail.com>
2021-06-03 00:56:29 +0200

    Refactoring api views 	- Update is not working 	- Need to add delete method too

--- metflix_remake/movies/api/views.py
+++ metflix_remake/movies/api/views.py
@@ -10,48 +10,32 @@ class MovieApiView(ViewSet):
 
     def list(self, request):
         movies = ModelMovieSerializer(Movie.objects.all(), many=True)
-        return Response(
-            data=movies.data, status=status.HTTP_200_OK
-        )
+        return Response(data=movies.data, status=status.HTTP_200_OK)
 
     def retrieve(self, request, pk: int):
-        movie = ModelMovieSerializer(
-            Movie.objects.get(pk=pk)
-        )
-        return Response(
-            data=movie.data, status=status.HTTP_200_OK
-        )
+        if pk is None:
+            return Response(request, status=status.HTTP_400_BAD_REQUEST)
+        movie_obj = Movie.objects.filter(id=pk).first()
+        if movie_obj is None:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        movie = ModelMovieSerializer(movie_obj)
+        return Response(data=movie.data, status=status.HTTP_200_OK)
 
     def create(self, request):
         movie = ModelMovieSerializer(data=request.POST)
         movie.is_valid(raise_exception=True)
-        data = {
-            'title': movie.validated_data.get('title'),
-            'nationality': movie.validated_data.get('nationality'),
-            'production_company': movie.validated_data.get('production_company'),
-            'year_release': movie.validated_data.get('year_release'),
-            # 'director': movie.validated_data['director'],
-            'budget': movie.validated_data.get('budget'),
-            'box_office': movie.validated_data.get('box_office'),
-            'running_time': movie.validated_data.get('running_time'),
-        }
-        new_movie = self.__save(data=data)
-        if new_movie is not None:
-            return self.retrieve(
-                request, pk=new_movie.id
-            )
+        if movie.is_valid():
+            movie_obj = movie.save()
+            return self.retrieve(request, pk=movie_obj.id)
+        return Response(movie.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    def __save(self, data=None):
-        movie = None
-        if data is not None:
-            movie = Movie.objects.create(
-                title=data.get('title', None),
-                nationality=data.get('nationality', None),
-                production_company=data.get('production_company', None),
-                year_release=data.get('year_release', None),
-                director=data.get('director', None),
-                budget=data.get('budget', None),
-                box_office=data.get('box_office', None),
-                running_time=data.get('running_time', None)
-            )
-        return movie
+    def update(self, request, pk=None):
+        movie_retrieved = self.retrieve(request, pk=pk)
+        if movie_retrieved is not None:
+            movie = Movie.objects.get(id=pk)
+            # TODO solve update  (PUT) (IS NOT WORKING JUST MAKING POST INSTEAD)
+            #movie_deserialized = ModelMovieSerializer(data=request.data)
+            """if movie_deserialized.is_valid():
+                movie_deserialized.up()
+                return Response(movie_deserialized.data, status=status.HTTP_200_OK)
+            return Response(movie_deserialized.errors, status=status.HTTP_400_BAD_REQUEST)"""
