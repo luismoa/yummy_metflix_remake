748e4757eb7a2a14166cebc1aced1b494d30b245
hikager <lluis22.ml@gmail.com>
2021-06-08 20:58:23 +0200

    Adding Director to the movies serializer 	- Adding then the Serializers for Director to make the instances 	of this object when is called from the MovieSerializer.

--- metflix_remake/movies/api/serializers.py
+++ metflix_remake/movies/api/serializers.py
@@ -1,18 +1,26 @@
-from typing import Dict
-
 from rest_framework import serializers
-from rest_framework.exceptions import ValidationError
+from ..models import Movie, Director
 
-from ..models import Movie
+
+class ModelDirectorSerializer(serializers.ModelSerializer):
+    id = serializers.IntegerField(read_only=True)
+    name_field = serializers.CharField(max_length=60, required=False)
+    nationality = serializers.CharField(required=False)
+
+    class Meta:
+        model = Director
+        fields = [
+            'id', 'name_field', 'nationality',
+        ]
 
 
 class ModelMovieSerializer(serializers.ModelSerializer):
     id = serializers.IntegerField(read_only=True)
-    title = serializers.CharField(max_length=60,required=False)
+    title = serializers.CharField(max_length=60, required=False)
     nationality = serializers.CharField(required=False)
     production_company = serializers.CharField(max_length=35, required=False)
     year_release = serializers.IntegerField(required=False)
-    # director = serializers.ForeignKey(Director, on_delete=models.CASCADE, blank=True, null=True)
+    director = ModelDirectorSerializer(many=False,required=False)
     budget = serializers.IntegerField(required=False)
     box_office = serializers.IntegerField(required=False)
     running_time = serializers.IntegerField(required=False)
